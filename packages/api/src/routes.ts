cedf99247853bb05da752efef85acc7a99a62b70
Natã <natan.danilo@gmail.com>
2023-01-07 23:25:00 -0300

    feat(googleapi): Consulta de dados de planilha

--- packages/api/src/routes.ts
+++ packages/api/src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import { createUserController } from './useCases/createUser';
+import { getProductListController } from './useCases/getProductList';
 
 const router = Router();
 
 router.post('/', (req, res) => {
   return createUserController.handle(req, res);
+});
+
+router.get('/', (req, res) => {
+  return getProductListController.handle(req, res);
 });
 
 export { router };
